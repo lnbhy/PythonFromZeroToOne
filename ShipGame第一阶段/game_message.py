a53af702cbd197b81330f91bcd7820fff6e06ddb
羡鱼肘子 <109861383+lnbhy@users.noreply.github.com>
2025-01-28 10:39:45 +0800

    Update game_message.py

--- ShipGame第一阶段/game_message.py
+++ ShipGame第一阶段/game_message.py
@@ -3,10 +3,11 @@ class GameMessage:
     '''用于游戏中的各种关键信息的统计'''
     def __init__(self, sq_game):
         '''初始化统计的游戏信息'''  
-        self.settinngs = sq_game.setting
+        self.settings = sq_game.setting
+        self.reset_stats()
     
     def reset_stats(self):
         '''游戏信息'''
-        self.ships_left = self.settinngs.ship_life
+        self.ship_life = self.settings.ship_life
         self.score = 0
         self.level = 1
